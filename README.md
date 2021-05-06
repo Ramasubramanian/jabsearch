eebc74e12cdc2b868f7dc2053d003c30c7bf2851
ramsankar <ramsankar@adfadmins-MacBook-Pro.local>
2021-05-06 11:53:00 +0530

    Base version

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # jabsearch
+
+A command line tool to check for 18+ vaccination slots in CoWin portal for a given pincode(s) or district. Just to automate the process of checking for slots in our preferred locations that is all. 
+
+Already there are services like under45.in, getjab.in etc that does this if you want a neat zero effort solution please register there. 
+
+However if you want to run this in your own machine and get notified you can use this utility.
+
+> REMEMBER: The tool uses [Government COWIN API](https://cdn-api.co-vin.in/api/v2), this information is as reliable as the API data
+
+**>>>> USE THIS AT YOUR OWN RISK<<<<<**
+
+### Disclaimer
+- This tool does not guarantee a vaccine slot it is just for checking whether a slot is available instead of using COWIN application UI to check frequently. 
+- **I am not responsible for any of the data or decisions you take out of this utility, you are on your own.** 
+
+- Due to speed of booking even if this utility says slots are available there is a possibility that the slots are booked before you login using OTP and try to book so please excuse if you are not able to find the slot in UI. 
+
+### Requirements
+1. Node JS version v15.5.0
+2. NPM version 7.3.0
+
+### Instructions to install
+- Clone the repo
+- Execute `npm install` command
+- Open `index.js` and change the first 6 lines as per your requirement
+```js
+const daysToCheckFor = 3;
+const pincodesToCheck = ['110001', '110002'];
+const vaccinesToCheck = ['COVISHIELD', 'COVAXIN'];
+const districtIdToCheck = 571;
+const minutesToCheckFor = 15;
+```
+- Please use the APIs to find [State IDs](https://cdn-api.co-vin.in/api/v2/admin/location/states) and [District IDs](https://cdn-api.co-vin.in/api/v2/admin/location/districts/31)
+- Run the command `npm start` 
+- You will get desktop notifications for every check performed for N minutes configured
---
+++ index.js
@@ -0,0 +1,155 @@
+//===================User config to be changed===================//
+const daysToCheckFor = 3;
+const pincodesToCheck = ['600095', '600100'];
+const vaccinesToCheck = ['COVISHIELD', 'COVAXIN'];
+const districtIdToCheck = 571;
+const minutesToCheckFor = 15;
+
+//====================Start of program DO NOT MODIFY UNLESS YOU KNOW WHAT YOU ARE DOING=====================
+
+const axios = require('axios');
+const moment = require('moment');
+const notifier = require('node-notifier');
+const cron = require('node-cron');
+
+let minutesToCheck = minutesToCheckFor < 15 ? 15 : minutesToCheckFor;
+
+axios.defaults.headers.common['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0";
+
+const baseUrl = 'https://cdn-api.co-vin.in/api/v2';
+const getByPinPath = 'appointment/sessions/public/calendarByPin';
+const getByDistrictPath = 'appointment/sessions/public/findByDistrict';
+
+function isObjectEligible(session) {
+    return session['min_age_limit'] === 18
+        && session['available_capacity'] > 0
+        && vaccinesToCheck.includes(session['vaccine']);
+}
+
+function findAvailableSlotsByCenter(slots) {
+    if (slots.length > 0) {
+        return slots.filter(slot => slot['centers'])
+            .flatMap(slot => slot['centers'])
+            .filter(center => center && center['sessions'])
+            .filter(center => {
+                let sessions = center['sessions']
+                    .filter(session => isObjectEligible(session));
+                let available = sessions.length > 0;
+                return available;
+            }).map(center => {
+                let centerData = {};
+                centerData['name'] = center['name'];
+                centerData['address'] = center['address'];
+                centerData['district_name'] = center['district_name'];
+                centerData['pincode'] = center['pincode'];
+                centerData['dates'] = center['sessions']
+                    .filter(session => isObjectEligible(session))
+                    .map(session => session['date']);
+                centerData['vaccines'] = [... new Set(center['sessions']
+                    .filter(session => isObjectEligible(session))
+                    .map(session => session['vaccine']))];
+                return centerData;
+            });
+    } else {
+        return [];
+    }
+}
+
+function findAvailableSlotsByDistrict(slots) {
+    if (slots.length > 0) {
+        return slots.filter(slot => slot['sessions'])
+            .flatMap(slot => slot['sessions'])
+            .filter(center => isObjectEligible(center))
+            .map(center => {
+                let centerData = {};
+                centerData['name'] = center['name'];
+                centerData['address'] = center['address'];
+                centerData['district_name'] = center['district_name'];
+                centerData['pincode'] = center['pincode'];
+                centerData['dates'] = [center['date']];
+                centerData['vaccines'] = [center['vaccine']];
+                return centerData;
+            });
+    } else {
+        return [];
+    }
+}
+
+function generateNextNDates(n) {
+    let today = new Date();
+    return [...Array(n).keys()]
+        .map(index => moment(today, 'DD-MM-YYYY').add(index, 'days'))
+        .map(date => moment(date).format('DD-MM-YYYY'))
+}
+
+async function getSlotsByPinCodes(pincodes) {
+    let promises = pincodes
+        .map(async pin => await getSlotsByPin(pin));
+    return await Promise.all(promises);
+}
+
+async function getSlotsByPin(pin) {
+    let promises = generateNextNDates(daysToCheckFor)
+        .map(async date => await getSlotsByPinAndDate(pin, date));
+    return await Promise.all(promises);
+}
+
+async function getSlotsByPinAndDate(pin, date) {
+    try {
+        const url = `${baseUrl}/${getByPinPath}?pincode=${pin}&date=${date}`;
+        console.log("Hitting URL:" + url)
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getSlotsByDistrict(dictrictId) {
+    let promises = generateNextNDates(daysToCheckFor)
+        .map(async date => await getSlotsByDistrictAndDate(dictrictId, date));
+    return await Promise.all(promises);
+}
+
+async function getSlotsByDistrictAndDate(districtId, date) {
+    try {
+        const url = `${baseUrl}/${getByDistrictPath}?district_id=${districtId}&date=${date}`;
+        console.log("Hitting URL:" + url)
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function main() {
+    let pinSlots = await getSlotsByPinCodes(pincodesToCheck);
+    let availableSlotsByPin = findAvailableSlotsByCenter(pinSlots.flat());
+    let districSlots = await getSlotsByDistrict(districtIdToCheck);
+    let availableSlotsByDistrict = findAvailableSlotsByDistrict(districSlots.flat());
+    let availableSlots = availableSlotsByPin.concat(availableSlotsByDistrict);
+    console.log("Available slots size: " + availableSlots.length);
+    if (availableSlots.length > 0) {
+        availableSlots.forEach(slot => console.log(slot));
+        availableSlots.slice(0, 2).forEach(slot => notifier.notify({
+            title: 'Vaccine Slots Available',
+            message: `Pincode: ${slot['pincode']}, Address: ${slot['address']}`,
+            timeout: 3600,
+            wait: true
+        }));
+    } else {
+        notifier.notify({
+            title: 'No Vaccine Slots Available',
+            message: `:-( Try later`,
+            timeout: 300,
+            wait: false
+        });
+    }
+}
+
+console.log("Started checking for vaccine slots @ " + new Date());
+main();
+cron.schedule(`*/${minutesToCheck} * * * *`, () => {
+    console.log("Started checking for vaccine slots @ " + new Date());
+    main();
+});
